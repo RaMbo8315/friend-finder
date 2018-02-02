8c8e32714958f0a734d0825f38a7f2f8c3290e7a
RaMbo8315 <henryurena@yahoo.com>
2018-02-02 01:52:44 -0500

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,50 +1,24 @@
 var friends = require("../data/friends");
 
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-    app.get("/api/friends", function (req, res) {
+    
+    app.get("/api/friends", (req, res) => {
         res.json(friends);
-        console.log(res)
     });
 
-    app.post("/api/friends", function (req, res) {
-
-        
-        // var a = [1, 2, 5, 2, 5, 4]
-        // var b = [2, 5, 4, 1, 5, 3]
+    app.post("/api/friends", (req, res) => {
         var diff = [];
         var totalDiff = [];
         var index = 0;
-        findMatch()
-
-        function findMatch() {
-            for (var i = 0; i < friends.length; i++) {
-                // var parsed = parseInt(friends[i].scores)
-                diff = friends[i].scores.map(function (item, ) {
-                    // In this case item correspond to currentValue of array a, 
-                    // using index to get value from array b
-
-                    return Math.abs(item - parseInt(req.body.scores));
-                })
-
-                const reducer = (accumulator, currentValue) => accumulator + currentValue;
-                totalDiff.push(diff.reduce(reducer))
-                console.log(totalDiff)
-            }
-
-            index = totalDiff.indexOf(Math.min.apply(null, totalDiff))
-            console.log(index)
+        for (var i = 0; i < friends.length; i++) {
+            diff = friends[i].scores.map((item) => {
+                return Math.abs(item - parseInt(req.body.scores));
+            })
+            var reducer = (accumulator, currentValue) => accumulator + currentValue;
+            totalDiff.push(diff.reduce(reducer))
         }
-        console.log(friends[index].photo)
-        // friends.push(req.body);
+        index = totalDiff.indexOf(Math.min.apply(null, totalDiff))
         friends.push(req.body)
         res.json(friends[index])
     });
-    // res.json(friends[index])
-    
-
 }
